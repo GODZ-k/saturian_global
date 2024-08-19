af98b9e67cc3e38f2d800c2c6a7e8b170160a1ea
Tanmay khatri <131422684+GODZ-k@users.noreply.github.com>
2024-08-19 10:58:35 +0530

    access

--- saturianglobal/settings.py
+++ saturianglobal/settings.py
@@ -55,10 +55,10 @@ EXTERNAL_APPS = [
 
 INSTALLED_APPS += EXTERNAL_APPS
 
-SECURE_SSL_REDIRECT = True  # it must be true in production
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+# SECURE_SSL_REDIRECT = True  # it must be true in production
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
